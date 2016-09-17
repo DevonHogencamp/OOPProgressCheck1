87905c7e091e396fa2e84748820f6e7c509b5fe0
Devon Hogencamp <devon.hogencamp@west-mec.org>
2016-09-16 18:18:14 -0700

    Added final instances of objects and called functions

--- index.php
+++ index.php
@@ -6,9 +6,22 @@
     </head>
     <body>
         <?php
+            // Link to the classes
             include 'class.php';
-
             
+            // Create Instance of Toyota Supra and Nissan Skyline
+            $toyotaSupra = new toyotaSupra;
+            $nissanSkyline = new nissanSkyline;
+
+            // Set the gear of Toyota Supra to first
+            setGear(1);
+            // Tells the Toyota Supra to speed up and get its speed in that gear
+            getSpeedUp();
+
+            // Pulls the handbrake of Nissan Skyline 
+            setHandbrake(true);
+            // Finds out if the Nissan Skyline is drifting
+            getDrift();
         ?>
     </body>
 </html>
