badc59b7e1c517dea02db9acf46f0cd793a42f02
Devon Hogencamp <devon.hogencamp@west-mec.org>
2016-09-16 18:38:17 -0700

    Final last minute change to the new objects

--- index.php
+++ index.php
@@ -10,16 +10,23 @@
             include 'class.php';
             
             // Create Instance of Toyota Supra and Nissan Skyline
-            $toyotaSupra = new toyotaSupra;
-            $nissanSkyline = new nissanSkyline;
+            $toyotaSupra = new toyotaSupra("Devon Hogencamp");
+            
+            // Its hardto give brandon his next dream car but I guess its just a code car I can make another one
+            $nissanSkyline = new nissanSkyline("Brandon Ching");
+            
+            // I made myself another nissan skyline
+            $devonsNissanSkyline = new nissanSkyline("Devon Hogencamp");
 
             // Set the gear of Toyota Supra to first
             setGear(1);
+            
             // Tells the Toyota Supra to speed up and get its speed in that gear
             getSpeedUp();
 
             // Pulls the handbrake of Nissan Skyline 
             setHandbrake(true);
+            
             // Finds out if the Nissan Skyline is drifting
             getDrift();
         ?>
