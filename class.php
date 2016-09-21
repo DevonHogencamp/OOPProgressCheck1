1988899fcf378ae18bcfb14a574b1081e9cbf592
Devon Hogencamp <devon.hogencamp@west-mec.org>
2016-09-21 15:24:13 -0700

    Added the static function and method

--- class.php
+++ class.php
@@ -1,4 +1,20 @@
 <?php
+    /*
+    *Using the same project you worked on in the previous progress check, incorporate a static property and method.
+
+    *This can be integrated or it can be new (i.e. comment out the previous code so that this property/method will work.
+
+    Incorporate at least one abstract class, with at least three inherited child classes, and at least two methods.
+
+    Create two separate files with a .php extension and include the autoloading function in your index file.
+
+    Ensure that these two separate classes are functioning within the index file that you will call them to.
+
+    Demonstrate your understanding of the Final keyword by altering the parent external class file and indicating its function with a comment.
+
+    Using one of your previous classes, iterate over the attributes of that class.
+    */
+
     // Created parent class JDMTuner
     class JDMTuner{
         // Set up all of the attributes
@@ -6,16 +22,26 @@
         public $model;
         public $engine;
         public $owner;
-        
+
         // Set up private attributes so that noboby knew the code name or vin #
-        private $codeName = "Super Godzilla";
+        private static $codeName = "Super Godzilla";
         public $vin;
-        
+
         // Used constructor to have person enter their name
         function __construct($name){
             $this->owner = $name;
         }
-        
+
+        // Validate that the user has put the right code name in
+        static function __construct($codeName){
+            if (self::$codeName = $codeName;) {
+                echo "You have entered code name correctly";
+            }
+            else {
+                echo "You have not entered the correct code name";
+            }
+        }
+
         // Sets the cars vin # to random
         function setVin(){
             $this->vin = rand();
@@ -29,30 +55,30 @@
         public $acceleration;
         public $gear = 2;
         public $speed = 50;
-        
+
         // Overided the manufacture and model that was inherited from parent class
         function __construct(){
             $this->manufacture = "Nissan";
             $this->model = "Skyline";
         }
-        
+
         // create setGear function that changes gear of car to whatever user wants
         function setGear($gear){
             // Brings in the accerleration base
             $this->accelerationBase = $accelerationBase;
-            
+
             // Brings in current gear
             $this->gear = $gear;
-            
+
             // Changes accelerations base on what gear you are in(the lower the gear the more acceleration)
             $this->acceleration = $accelerationBase / $gear;
         }
-        
+
         // create getSpeed funcion that calculates speed and returns speed
         function getSpeedUp(){
             // Calculates speed
             $this->speed = $speed + $acceleration;
-            
+
             // returns speed
             return $speed;
         }
@@ -63,26 +89,26 @@
         // Set the cars public attributes
         public $handbrake = false;
         public $grip = rand();
-        
+
         // Overides the manufacture and model from inheritance of parent JDMTuner
         function __construct(){
             $this->manufacture = "Nissan";
             $this->model = "Skyline";
         }
-        
+
         // Return wether the car is drifting or not
         function getDrift(){
             // If the handbrake is engaged then the car is drifting
             if ($handbrake == true) {
                 return "Drift Succesfull";
             }
-            
+
             // If the handbrake is not engaged then the car is not drifting
             else {
                 return "Set that handbrake";
             }
         }
-        
+
         // sets the handbrake to what the user specifies
         function setHandbrake($change){
             $this->handbrake = $change;
--- index.php
+++ index.php
@@ -6,27 +6,43 @@
     </head>
     <body>
         <?php
+            /*
+            Using the same project you worked on in the previous progress check, incorporate a static property and method.
+
+            This can be integrated or it can be new (i.e. comment out the previous code so that this property/method will work.
+
+            Incorporate at least one abstract class, with at least three inherited child classes, and at least two methods.
+
+            Create two separate files with a .php extension and include the autoloading function in your index file.
+
+            Ensure that these two separate classes are functioning within the index file that you will call them to.
+
+            Demonstrate your understanding of the Final keyword by altering the parent external class file and indicating its function with a comment.
+
+            Using one of your previous classes, iterate over the attributes of that class.
+            */
+
             // Link to the classes
             include 'class.php';
-            
+
             // Create Instance of Toyota Supra and Nissan Skyline
             $toyotaSupra = new toyotaSupra("Devon Hogencamp");
-            
+
             // Its hardto give brandon his next dream car but I guess its just a code car I can make another one
             $nissanSkyline = new nissanSkyline("Brandon Ching");
-            
+
             // I made myself another nissan skyline
             $devonsNissanSkyline = new nissanSkyline("Devon Hogencamp");
 
             // Set the gear of Toyota Supra to first
             setGear(1);
-            
+
             // Tells the Toyota Supra to speed up and get its speed in that gear
             getSpeedUp();
 
-            // Pulls the handbrake of Nissan Skyline 
+            // Pulls the handbrake of Nissan Skyline
             setHandbrake(true);
-            
+
             // Finds out if the Nissan Skyline is drifting
             getDrift();
         ?>
